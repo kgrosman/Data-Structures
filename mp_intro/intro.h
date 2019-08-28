51ec43cbb4342bbfb11d7e95d84bbd34261d7857
kgrosman <42973295+kgrosman@users.noreply.github.com>
2019-08-28 19:25:29 -0400

    Provided Code

---
+++ mp_intro/intro.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "cs225/PNG.h"
+
+#include <string>
+
+
+void rotate(std::string inputFile, std::string outputFile);
+cs225::PNG myArt(unsigned int width, unsigned int height);
+
+
